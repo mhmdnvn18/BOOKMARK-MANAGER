68ad346070e71c27457ac0707d09a8b74ebf3838
anop <81241987+mhmdnvn18@users.noreply.github.com>
2025-03-06 03:46:47 +0700

    update

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+
+exports.getFirebaseConfig = functions.https.onRequest((req, res) => {
+    const firebaseConfig = {
+        apiKey: "YOUR_API_KEY",
+        authDomain: "YOUR_AUTH_DOMAIN",
+        projectId: "YOUR_PROJECT_ID",
+        storageBucket: "YOUR_STORAGE_BUCKET",
+        messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+        appId: "YOUR_APP_ID",
+        measurementId: "YOUR_MEASUREMENT_ID"
+    };
+    res.json(firebaseConfig);
+});
